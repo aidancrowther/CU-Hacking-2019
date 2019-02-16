f6b7abf2880c2ed9928b68b92ab2f13f8da09cd8
Tess <>
2019-02-16 18:55:15 -0500

    screaming

--- server/Public/map.js
+++ server/Public/map.js
@@ -29,10 +29,54 @@
 		fillOpacity: 0.3,
 		map: map,
 		center: map.center,
-		radius: 1000
-		
-		
+		radius:  1250
 	});
+	
+	//NONE OF THIS WORKS
+	/*
+	 // Define the LatLng coordinates for the polygon's  outer path.
+  var outerCoords = [
+    map.getBounds
+  ];
+
+  // Define the LatLng coordinates for the polygon's inner path.
+  // Note that the points forming the inner path are wound in the
+  // opposite direction to those in the outer path, to form the hole.
+  var innerCoords = [
+    {lat: 28.745, lng: -70.579},
+    {lat: 29.570, lng: -67.514},
+    {lat: 27.339, lng: -66.668}
+  ];
+
+  // Construct the polygon, including both paths.
+  var deathArea = new google.maps.Polygon({
+    paths: [outerCoords, innerCoords],
+    strokeColor: '#FFC107',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FFC107',
+    fillOpacity: 0.35,
+	map: map
+  });
+	*/	
+	
+	
+	/*
+
+	var deathArea = new google.maps.({	
+		strokeColor: '#FE7569',
+		strokeOpacity: 0.8,
+		strokeWeight: 2,
+		fillColor: '#FE7569',
+		fillOpacity: 0.35,
+		map: map,
+		//paths: [map.getBounds(), deathCircle.getBounds()]
+		bounds: map.getBounds()
+	});
+	
+	*/
+	
+
 	
   }
 
