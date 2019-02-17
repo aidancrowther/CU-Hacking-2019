7286170761aa3b67a47d67344649ec30dfd2dd62
Tess <>
2019-02-17 02:45:13 -0500

    added in game code and user list to waiting room, made them appear pretty

--- server/Public/map.js
+++ server/Public/map.js
@@ -1,8 +1,8 @@
-  var map;
-  var deathCircle;
-  function initMap() {
+var map;
+var deathCircle;
+function initMap() {
     // Create the map object.
-    map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 14,
 		scaleControl: true
 		
@@ -34,92 +34,34 @@
 		radius: 1250
 	});
 	
-
-	
-	//NONE OF THIS WORKS
-	/*
-	 // Define the LatLng coordinates for the polygon's  outer path.
-  var outerCoords = [
-    map.getBounds
-  ];
-
-  // Define the LatLng coordinates for the polygon's inner path.
-  // Note that the points forming the inner path are wound in the
-  // opposite direction to those in the outer path, to form the hole.
-  var innerCoords = [
-    {lat: 28.745, lng: -70.579},
-    {lat: 29.570, lng: -67.514},
-    {lat: 27.339, lng: -66.668}
-  ];
-
-  // Construct the polygon, including both paths.
-  var deathArea = new google.maps.Polygon({
-    paths: [outerCoords, innerCoords],
-    strokeColor: '#FFC107',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#FFC107',
-    fillOpacity: 0.35,
-	map: map
-  });
-	*/	
-	
-	
-	/*
-
-	var deathArea = new google.maps.({	
-		strokeColor: '#FE7569',
-		strokeOpacity: 0.8,
-		strokeWeight: 2,
-		fillColor: '#FE7569',
-		fillOpacity: 0.35,
-		map: map,
-		//paths: [map.getBounds(), deathCircle.getBounds()]
-		bounds: map.getBounds()
-	});
-	
-	*/
-	
   }
   
-	function shrinkCircle(meters){	  
-	 //speed of shrinking needs to be constant
-	 //2 meters per 50 milliseconds 
-		shrinkCircleRecursive((meters/2)); 
-	}
+function shrinkCircle(meters){	  
+//speed of shrinking needs to be constant
+//2 meters per 50 milliseconds 
+	shrinkCircleRecursive((meters/2)); 
+}
 	
-	function shrinkCircleRecursive(count){
-		
-		if(count < 0) return;
-		deathCircle.setRadius(deathCircle.radius - 2);
-		setTimeout(shrinkCircleRecursive, 50, count-1);
+function shrinkCircleRecursive(count){
+	if(count < 0) return;
+	deathCircle.setRadius(deathCircle.radius - 2);
+	setTimeout(shrinkCircleRecursive, 50, count-1);
+}
 	
-		//let textDiv = document.getElementById("text-area");
-		//textDiv.innerHTML = deathCircle.radius.toString();
-	
-	
-
-	/*
-		for (var i = 0; i < 10; ++i){
-			deathCircle.setRadius(deathCircle.radius - 10);
-		} 
-		*/
-	}
-	
-	function updateData(){
-		let leftAlive = document.getElementById("stillAlive");
-		leftAlive.innerHTML = "Still Alive: " + "100";
+function updateData(){
+	let leftAlive = document.getElementById("stillAlive");
+	leftAlive.innerHTML = "Still Alive: " + "100";
 		
-		let yourTarget = document.getElementById("targetPerson");
+	let yourTarget = document.getElementById("targetPerson");
+	yourTarget.innerHTML = "targetname"
 		
-		let playersInLobby = document.getElementById("playersJoined");
-		playerInLobby.innerHTML = "Players in Lobby: " + "69";
+	let playersInLobby = document.getElementById("playersJoined");
+	playerInLobby.innerHTML = "Players in Lobby: " + "69";
 		
-		let listOfPlayers = document.getElementById("playerList");
-		listOfPlayers.innerHTML = "playername" + " <br> "
+	let listOfPlayers = document.getElementById("playerList");
+	listOfPlayers.innerHTML = "playername" + " <br> "
 		
-		
-	}
+}
 
 	  
  
