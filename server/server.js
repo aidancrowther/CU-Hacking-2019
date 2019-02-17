2ca663396524b5a66427f2cd9554148f75a0cf4f
Benjamin Karstad <benkarstad@gmail.com>
2019-02-17 05:59:20 -0500

    unfuck the determineDamage() indexing oopsie

--- server/server.js
+++ server/server.js
@@ -157,7 +157,7 @@ function determineDamage(id){
     for(let playerKey in players){
         let player = players[playerKey];
         let playerIndex = list.indexOf(playerKey);
-        let hunter = list[(playerIndex+list.length-1)%length];
+        let hunter = list[(playerIndex+list.length-1)%list.length];
         
         //damage from hunter
         if(!outsideRange(player["location"], hunter["location"], hunterDmgDist))
